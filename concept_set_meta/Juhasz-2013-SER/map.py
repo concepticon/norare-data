090a27b9dd8a77bec20442814ea190d92c6c93df
Annika Tjuka <annika.tjuka@student.hu-berlin.de>
2020-04-23 17:36:48 +0200

    Add Juhasz-2013-SER

---
+++ concept_set_meta/Juhasz-2013-SER/map.py
@@ -0,0 +1,25 @@
+from pynorare.dataset import NormDataSet
+from sys import argv
+
+class Dataset(NormDataSet):
+
+    id = "Juhasz-2013-SER"
+
+    def download(self):
+        self.download_file(
+            'https://static-content.springer.com/esm/art%3A10.3758%2Fs13428-012-0242-9/MediaObjects/13428_2012_242_MOESM1_ESM.xls',
+            '13428_2012_242_MOESM1_ESM.xls'
+        )
+
+    def map(self, write_file=True):
+        
+        sheet = self.get_excel('13428_2012_242_MOESM1_ESM.xls', 0, dicts=True)
+        self.extract_data(
+                sheet,
+                gloss='ENGLISH',
+                language='en',
+                write_file=write_file)
+
+                
+if __name__ == '__main__':
+    Dataset().run(argv)
