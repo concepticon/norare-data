a1d0e8a08dc43dd33f810d98c07c46588a8bd7fc
Annika Tjuka <annika.tjuka@student.hu-berlin.de>
2020-04-29 15:10:37 +0200

    Update Brysbaert-2009

---
+++ concept_set_meta/Brysbaert-2009-SUBTLEXUS/map.py
@@ -0,0 +1,24 @@
+from pynorare.dataset import NormDataSet
+
+class Dataset(NormDataSet):
+
+    id = "Brysbaert-2009-SUBTLEXUS"
+
+    def download(self):
+        self.download_zip(
+            'https://static-content.springer.com/esm/art%3A10.3758%2FBRM.41.4.977/MediaObjects/Brysbaert-BRM-2009.zip',
+            'brysbaert_freq',
+            'Brysbaert-BRM-2009/SUBTLEXusExcel2007.xlsx',
+        )
+
+    def map(self, write_file=True):
+        
+        sheet = self.get_excel('Brysbaert-BRM-2009/SUBTLEXusExcel2007.xlsx', 0,
+                dicts=True)
+
+        self.extract_data(
+                sheet,
+                gloss='ENGLISH',
+                language='en',
+                write_file=write_file)
+
