f819cb964c63688ae0c4dca90c2ad868223445b4
lingulist <mattis.list@lingpy.org>
2020-05-20 19:04:12 +0200

    update

---
+++ concept_set_meta/Bond-2013-OMW/map.py
@@ -0,0 +1,33 @@
+from pynorare.dataset import NormDataSet
+import zipfile
+from nltk.corpus import wordnet as wn
+
+class Dataset(NormDataSet):
+
+    id = "Bond-2013-OMW"
+
+    def download(self):
+        # download synset
+        self.download_file(
+                'http://compling.hss.ntu.edu.sg/omw/wn30-core-synsets.tab',
+                'core-synsets.tsv',
+        )
+        # download nltk stuff
+        self.download_file(
+                'https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/corpora/wordnet.zip',
+                'wordnet.zip'
+                )
+
+    def map(self, write_file=True):
+        
+        sheet = self.get_csv('wordnet.tsv')
+
+        self.extract_data(
+                sheet,
+                pos=True,
+                pos_mapper = {
+                    'v': 'Action/Process',
+                    'n': 'Person/Thing',
+                    'a': 'Property'}
+                )
+
