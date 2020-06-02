da043499b3da36e9aeff65fccd8097a31bdac059
Annika Tjuka <annika.tjuka@student.hu-berlin.de>
2020-06-02 13:23:59 +0200

    Fix typo

--- concept_set_meta/Scott-2019-Ratings/map.py
+++ concept_set_meta/Scott-2019-Ratings/map.py
@@ -14,31 +14,6 @@ class Dataset(NormDataSet):
 
         self.extract_data(
             sheet,
-            {
-                ('Words', 'ENGLISH', str),
-                ('AROU', "ENGLISH_AROUSAL_MEAN", str),
-                ('VAL', 'ENGLISH_VALENCE_MEAN', str),
-                ('DOM', "ENGLISH_DOMINANCE_MEAN", str),
-                ('CNC', "ENGLISH_CONCRETENESS_MEAN", str),
-                ('IMAG', "ENGLISH_IMAGEABILITY_MEAN", str),
-                ('FAM', "ENGLISH_FAMILIARITY_MEAN", str),
-                ('AOA', "ENGLISH_AOA_MEAN", str),
-                ('SIZE', "ENGLISH_SEM_SIZE_MEAN", str),
-                ('GEND', "ENGLISH_GENDER_ASSOCIATION_MEAN", str)},
-            [
-                'CONCEPTICON_ID',
-                'CONCEPTICON_GLOSS',
-                'ENGLISH',
-                "ENGLISH_AROUSAL_MEAN",
-                "ENGLISH_VALENCE_MEAN",
-                "ENGLISH_DOMINANCE_MEAN",
-                "ENGLISH_CONCRETENESS_MEAN",
-                "ENGLISH_IMAGEABILITY_MEAN",
-                "ENGLISH_FAMILIARITY_MEAN",
-                "ENGLISH_AOA_MEAN",
-                "ENGLISH_SEM_SIZE_MEAN",
-                "ENGLISH_GENDER_ASSOCIATION_MEAN"
-            ],
                 gloss='ENGLISH',
                 language='en',
                 write_file=write_file)
