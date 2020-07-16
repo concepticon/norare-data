5cb6b4eaceced789bb8cd92ba8ae3c764bf07a59
Annika <annika.tjuka@gmail.com>
2020-07-16 17:26:25 +0200

    remappings

--- concept_set_meta/Tsang-2018-LexicalDecision/map.py
+++ concept_set_meta/Tsang-2018-LexicalDecision/map.py
@@ -16,5 +16,5 @@ class Dataset(NormDataSet):
         self.extract_data(
                 sheet,
                 gloss='CHINESE',
-                language='zh',
-                write_file=write_file)
+                language='zh'
+                )
