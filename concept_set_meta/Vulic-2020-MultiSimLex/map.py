e08b3d30e0c7848af7e08b0e205dc23892e72594
Annika <annika.tjuka@gmail.com>
2020-11-11 16:18:44 +0100

    Add MultiSimLex

---
+++ concept_set_meta/Vulic-2020-MultiSimLex/map.py
@@ -0,0 +1,20 @@
+from pynorare.dataset import NormDataSet
+
+class Dataset(NormDataSet):
+
+    id = "Vulic-2020-MultiSimLex"
+
+    def download(self):
+        self.download_file(
+            'https://multisimlex.com/data/scores.xlsx',
+            'scores.xlsx',
+        )
+
+    def map(self, write_file=True):
+        
+        sheet = self.get_csv('scores-with-translations.tsv', dicts=True)
+        self.extract_data(
+                sheet,
+                gloss='ENGLISH',
+                language='en'
+                )
