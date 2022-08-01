3f95afe53dd23c24c8245402026ae96da6f0913c
Robert Forkel <xrotwang@googlemail.com>
2022-08-01 17:52:04 +0200

    turns out the type was the only problem ...

--- concept_set_meta/Speed-2021-Sensorimotor/map.py
+++ concept_set_meta/Speed-2021-Sensorimotor/map.py
@@ -1,7 +1,3 @@
-import subprocess
-
-from csvw.dsv import reader, UnicodeWriter
-
 from pynorare.dataset import NormDataSet
 
 
@@ -14,13 +10,9 @@ class Dataset(NormDataSet):
             'https://osf.io/wzfpd/download',
             'SpeedBrysbaert_Norms.xlsx',
         )
-        subprocess.check_call('libreoffice --headless --convert-to csv {0}/SpeedBrysbaert_Norms.xlsx --outdir {0}'.format(self.raw_dir).split())
-        rows = list(reader(self.raw_dir / 'SpeedBrysbaert_Norms.csv', encoding='cp1252'))
-        with UnicodeWriter(self.raw_dir / 'SpeedBrysbaert_Norms.csv') as w:
-            w.writerows(rows)
 
     def map(self, write_file=True):
-        sheet = self.get_csv('SpeedBrysbaert_Norms.csv', ',', dicts=True)
+        sheet = self.get_excel('SpeedBrysbaert_Norms.xlsx', 0, dicts=True)
         self.extract_data(
             sheet,
             gloss='DUTCH',
