1db2941215f170b4c56bc8970ba4f691d1910e40
Robert Forkel <xrotwang@googlemail.com>
2022-08-01 17:48:10 +0200

    closes #185

--- concept_set_meta/Speed-2021-Sensorimotor/map.py
+++ concept_set_meta/Speed-2021-Sensorimotor/map.py
@@ -1,4 +1,9 @@
+import subprocess
+
+from csvw.dsv import reader, UnicodeWriter
+
 from pynorare.dataset import NormDataSet
+
 
 class Dataset(NormDataSet):
 
@@ -9,20 +14,23 @@ class Dataset(NormDataSet):
             'https://osf.io/wzfpd/download',
             'SpeedBrysbaert_Norms.xlsx',
         )
+        subprocess.check_call('libreoffice --headless --convert-to csv {0}/SpeedBrysbaert_Norms.xlsx --outdir {0}'.format(self.raw_dir).split())
+        rows = list(reader(self.raw_dir / 'SpeedBrysbaert_Norms.csv', encoding='cp1252'))
+        with UnicodeWriter(self.raw_dir / 'SpeedBrysbaert_Norms.csv') as w:
+            w.writerows(rows)
 
     def map(self, write_file=True):
-        
-        sheet = self.get_excel('SpeedBrysbaert_Norms.xlsx', 0, dicts=True)
+        sheet = self.get_csv('SpeedBrysbaert_Norms.csv', ',', dicts=True)
         self.extract_data(
-                sheet,
-                gloss='DUTCH',
-                language='nl',
-                pos=True,
-                pos_mapper = {
-                    'N': 'Person/Thing',
-                    'ADJ': 'Property',
-                    'WW': 'Action/Process',
-                    'Function': 'Other',
-                    "TW": "Number"},
-                pos_name = "DUTCH_POS"
-                )
+            sheet,
+            gloss='DUTCH',
+            language='nl',
+            pos=True,
+            pos_mapper = {
+                'N': 'Person/Thing',
+                'ADJ': 'Property',
+                'WW': 'Action/Process',
+                'Function': 'Other',
+                "TW": "Number"},
+            pos_name = "DUTCH_POS"
+        )
