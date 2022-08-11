020118627efda8c665403768fee0ce89b2a84933
Robert Forkel <xrotwang@googlemail.com>
2022-08-11 08:26:38 +0200

    use nltk in download and map for OMW

--- datasets/Bond-2013-OMW/norare.py
+++ datasets/Bond-2013-OMW/norare.py
@@ -1,17 +1,20 @@
+from collections import defaultdict
+
+from csvw.dsv import reader, UnicodeWriter
+import nltk
+
+
 def download(dataset):
     dataset.download_file(
         'http://compling.hss.ntu.edu.sg/omw/wn30-core-synsets.tab',
         'core-synsets.tsv',
     )
-    # download nltk stuff
-    #dataset.download_file(
-    #    'https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/corpora/wordnet.zip',
-    #    'wordnet.zip'
-    #        )
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+    preprocess(dataset.raw_dir)
 
 
 def map(dataset, concepticon, mappings):
-    sheet = dataset.get_csv('wordnet.tsv')
     dataset.extract_data(
         'wordnet.tsv',
         concepticon,
@@ -22,4 +25,42 @@ def map(dataset, concepticon, mappings):
             'n': 'Person/Thing',
             'a': 'Property'}
     )
+
+
+def preprocess(d):
+    from nltk.corpus import wordnet as wn
+
+    core = [x[0] for x in reader(d / 'core-synsets.tsv')]
+
+    def name(ss):
+        return str(ss._offset).rjust(8, '0')+'-'+ss._pos
+
+    sheet = [
+        'word', 'pos', 'wordnet_id', 'nltk_name', 'hypernyms', 'hyponyms',
+        'hypernym_names', 'hyponym_names', 'in_degree', 'out_degree']
+    with UnicodeWriter(d / 'wordnet.tsv', delimiter='\t') as w:
+        w.writerow(sheet)
+        for row in core:
+            ss = wn.of2ss(row)
+            lemma_name = ss.lemma_names()[0]
+            # get hyponyms
+            hpn = []
+            for h in ss.hypernyms():
+                hpn += [(name(h), h._name)]
+            hon = []
+            for h in ss.hyponyms():
+                hon += [(name(h), h._name)]
+    
+            w.writerow([
+                lemma_name,
+                ss._pos,
+                row,
+                ss._name,
+                ';'.join([x[0] for x in hpn]),
+                ';'.join([x[0] for x in hon]),
+                ';'.join([x[1] for x in hpn]),
+                ';'.join([x[1] for x in hon]),
+                str(len(hpn)),
+                str(len(hon))
+            ])
 
