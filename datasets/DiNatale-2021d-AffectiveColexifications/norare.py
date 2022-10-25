8e5bc7562b7b02696635561d15a5e4bc14b1d13b
Annika <annika.tjuka@gmail.com>
2022-10-25 15:59:52 +0200

    Add DiNatale-2021d-AffectiveColexifications

---
+++ datasets/DiNatale-2021d-AffectiveColexifications/norare.py
@@ -0,0 +1,17 @@
+def download(dataset):
+    dataset.download_zip(
+        'https://github.com/AnnaDiNatale/colex_affective/archive/refs/heads/master.zip',
+        'colex_affective-master.zip',
+        'colex_affective-master/lexica/freedict_WKB.csv'
+    )
+
+
+def map(dataset, concepticon, mappings):
+    dataset.extract_data(
+        dataset.get_csv('colex_affective-master/lexica/freedict_WKB.csv', delimiter=","),
+        concepticon,
+        mappings,
+        gloss='ENGLISH',
+        language='en'
+    )
+
