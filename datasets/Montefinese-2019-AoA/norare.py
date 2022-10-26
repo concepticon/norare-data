4724006e751994e2ef7730ace95755f2614bbc9f
Annika <annika.tjuka@gmail.com>
2022-10-26 10:54:04 +0200

    Add /Montefinese-2019-AoA

--- datasets/Montefinese-2019-AoA/norare.py
+++ datasets/Montefinese-2019-AoA/norare.py
@@ -5,17 +5,19 @@ def download(dataset):
     )
 
 
-def map(dataset, concepticon, mappings):    
-    sheet = dataset.get_excel('ItAoA.xlsx', 1, dicts=False)
+def map(dataset, concepticon, mappings):  
+    sheet_list = dataset.get_excel('ItAoA.xlsx', 1, dicts=False)
+    sheet = [dict(zip(sheet_list[0], row)) for row in sheet_list[1:]]
+
     dataset.extract_data(
         sheet,
         concepticon,
         mappings,
-        pos=True,
         pos_mapper = {
             'n': 'Person/Thing',
             'a': 'Property',
             'v': 'Action/Process'},
-        pos_name = "ITALIAN_POS"
+        pos_name = "ITALIAN_POS",
+        gloss='ENGLISH',
+        language='en'
     )
-
