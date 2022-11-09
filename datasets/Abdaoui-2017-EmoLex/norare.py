4d6090515ea5422570ed62651ec58240b2a5e66a
Robert Forkel <xrotwang@googlemail.com>
2022-11-09 17:20:05 +0100

    Boolean values must be passed to `extract_data` as booleans
    We must take care to pass values for variables specified as boolean
    as boolean - rather than string - into `extract_data`, because otherwise
    each non-empty string will be interpreted as `True`.

--- datasets/Abdaoui-2017-EmoLex/norare.py
+++ datasets/Abdaoui-2017-EmoLex/norare.py
@@ -4,4 +4,8 @@ def download(dataset):
 
 def map(dataset, concepticon, mappings):    
     sheet = dataset.get_csv('FEEL.csv', delimiter=";", dicts=True)
+    bools = 'joy;fear;sadness;anger;surprise;disgust'.split(';')
+    sheet = [
+        {k: (True if v == '1' else (False if v == '0' else None)) if k in bools else v for k, v in row.items()}
+        for row in sheet]
     dataset.extract_data(sheet, concepticon, mappings, gloss='FRENCH', language='fr')
