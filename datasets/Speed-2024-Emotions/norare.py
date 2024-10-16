7dbb82e622d7ae3cf5a01e8f8dd97b330ca12dd4
MiraAhmedovic <mira_ahmedovic@eva.mpg.de>
2024-10-16 11:28:43 +0200

    tweaking files to find error

--- datasets/Speed-2024-Emotions/norare.py
+++ datasets/Speed-2024-Emotions/norare.py
@@ -1,7 +1,8 @@
- def download(dataset):
+def download(dataset):
     dataset.download_file(
         'https://osf.io/download/h76zj/',
-    )     
+        'SpeedBrysbaertEmotionNorms.xlsx',
+    )
 
 def map(dataset, concepticon, mappings):    
     dataset.extract_data(
