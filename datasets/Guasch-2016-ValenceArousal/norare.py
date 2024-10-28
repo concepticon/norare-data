63180a84791791d800ab1fd4e5c5a9cdd548a2f2
MiraAhmedovic <mira_ahmedovic@eva.mpg.de>
2024-10-28 13:02:54 +0100

    adding Guasch-2016-ValenceArousal

---
+++ datasets/Guasch-2016-ValenceArousal/norare.py
@@ -0,0 +1,14 @@
+def download(dataset):
+    dataset.download_file(
+        'https://static-content.springer.com/esm/art%3A10.3758%2Fs13428-015-0684-y/MediaObjects/13428_2015_684_MOESM1_ESM.xlsx',
+        '13428_2015_684_MOESM1_ESM.xlsx',
+    )
+
+def map(dataset, concepticon, mappings):    
+    dataset.extract_data(
+        '13428_2015_684_MOESM1_ESM.xlsx',
+        concepticon,
+        mappings,
+        gloss='SPANISH',
+        language='es'
+    )          
