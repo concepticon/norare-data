9b349232ca1f5f3f5d01abb09cc9cdba58b639c3
MiraAhmedovic <mira_ahmedovic@eva.mpg.de>
2024-11-27 15:29:09 +0100

    setup dataset, not mapped yet

---
+++ datasets/Coso-2023-Emotions/norare.py
@@ -0,0 +1,17 @@
+def download(dataset):
+    dataset.download_file(
+        'https://figshare.com/ndownloader/files/36434421',
+        'CROWD-5e.xlsx',
+    )
+
+def map(dataset, concepticon, mappings):   
+    sheet_list = dataset.get_excel('CROWD-5e.xlsx', 1, dicts=False)
+    sheet = [dict(zip(sheet_list[0], row)) for row in sheet_list[1:]] 
+
+    dataset.extract_data(
+        'CROWD-5e.xlsx',
+        concepticon,
+        mappings,
+        gloss='CROATIAN',
+        language='hrv',
+    )
