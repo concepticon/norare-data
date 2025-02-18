93ab33e35408381bfb2ffca4983f6051e848f387
MiraAhmedovic <mira_ahmedovic@eva.mpg.de>
2025-02-18 09:01:37 +0100

    mapped dataset

--- datasets/Dingemanse-2020-IconicityHumor/norare.py
+++ datasets/Dingemanse-2020-IconicityHumor/norare.py
@@ -1,13 +1,13 @@
 def download(dataset):
     dataset.download_zip(
-        'https://github.com/mdingemanse/playful_iconicity/tree/refs/heads/master.zip',
-        'playful_iconicity-master.zip'
+        'https://github.com/mdingemanse/playful_iconicity/archive/refs/heads/master.zip',
+        'playful_iconicity-master.zip',
         'playful_iconicity-master/data/combined-experimental-norms-with-humour-iconicity-aversion-taboo-predictions-logletterfreq.csv',
     )
 
 def map(dataset, concepticon, mappings):    
     dataset.extract_data(
-        dataset.get_csv('combined-experimental-norms-with-humour-iconicity-aversion-taboo-predictions-logletterfreq.csv', delimiter=","),
+        dataset.get_csv('playful_iconicity-master/data/combined-experimental-norms-with-humour-iconicity-aversion-taboo-predictions-logletterfreq.csv', delimiter=","),
         concepticon,
         mappings,
         gloss='ENGLISH',
