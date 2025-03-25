185fee5d47cec9fdc21924f3995ac45bcd8994a7
Mira A <mira.ahmedovic@gmail.com>
2025-03-25 11:52:13 +0100

    mapped dataset

--- datasets/Ljubesic-2020-Emotions/norare.py
+++ datasets/Ljubesic-2020-Emotions/norare.py
@@ -1,13 +1,13 @@
 def download(dataset):
     dataset.download_zip(
-        'https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1318/LiLaH-HR-NL-SL.zip?sequence=6&isAllowed=y'
+        'https://www.clarin.si/repository/xmlui/bitstream/handle/11356/1318/LiLaH-HR-NL-SL.zip?sequence=6&isAllowed=y',
         'LiLaH-HR-NL-SL.zip',
         'LiLaH-HR-NL-SL/LiLaH-HR-NL-SL.tsv',
     )
 
 def map(dataset, concepticon, mappings):
     dataset.extract_data(
-        dataset.get_csv('LiLaH-HR-NL-SL/LiLaH-HR-NL-SL.tsv', delimiter= "\t", dicts = True, coding = 'utf-8-sig'),
+        dataset.get_csv('LiLaH-HR-NL-SL/LiLaH-HR-NL-SL.tsv', delimiter="\t", dicts=True),
         concepticon,
         mappings,
         gloss='DUTCH',
