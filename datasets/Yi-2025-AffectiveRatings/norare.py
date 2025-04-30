a58582c8e1766281e2fe4bc5ab2cc84ffa74298c
Mira A <mira.ahmedovic@gmail.com>
2025-04-30 12:08:43 +0200

    changed NA values to actual null values"

--- datasets/Yi-2025-AffectiveRatings/norare.py
+++ datasets/Yi-2025-AffectiveRatings/norare.py
@@ -3,10 +3,15 @@ def download(dataset):
         'https://osf.io/download/836th/?view_only=4a67691a0fef40a9b8f8ec7296c1e1da',
         'emotion.en.ch.csv'
     )
+def replace_null(v):
+    return None if v in {'NA', '#N/A'} else v
 
 def map(dataset, concepticon, mappings):
+    rows = dataset.get_csv('emotion.en.ch.csv', delimiter=",", dicts=True)
+    cleaned_rows = [{k: replace_null(v) for k, v in row.items()} for row in rows]
+
     dataset.extract_data(
-        dataset.get_csv('emotion.en.ch.csv', delimiter=","),
+        cleaned_rows,
         concepticon,
         mappings,
         gloss='CHINESE',
@@ -14,5 +19,4 @@ def map(dataset, concepticon, mappings):
         pos=True,
         pos_mapper={'noun': 'Person/Thing', 'verb': 'Action/Process', 'adjective': 'Property'},
         pos_name="CHINESE_POS"
-    )       
-
+    )
