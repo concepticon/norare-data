8180e7b47a52184b1eb7ccb3164c0cd87dbedcb6
Mira A <mira.ahmedovic@gmail.com>
2025-09-29 15:22:06 +0200

    changing Brysbaert-2014-Concreteness to Brysbaert-2014a-Concreteness, adding Brysbaert-2014b-Aoa and Brysbaert-2014c-Concreteness

--- datasets/Brysbaert-2014a-Concreteness/norare.py
+++ datasets/Brysbaert-2014a-Concreteness/norare.py
@@ -9,24 +9,7 @@ def map(dataset, concepticon, mappings):
         '13428_2013_403_MOESM1_ESM.xlsx',
         concepticon,
         mappings,
-        pos=True,
-        pos_mapper = {
-            'Noun': 'Person/Thing',
-            'Adjective': 'Property',
-            'Verb': 'Action/Process',
-            'Adverb': 'Other',
-            'Article': 'Other',
-            'Conjunction': 'Other',
-            'Determiner': "Other",
-            'Ex': "Other",
-            '#N/A': 'Other',
-            "Interjection": "Other",
-            "Name": "Person/Thing",
-            "Preposition": "Other",
-            "To": "Other",
-            "Number": "Number",
-            "Pronoun": "Other",
-            "Not": "Other"},
-        pos_name = "ENGLISH_POS"
+        gloss='ENGLISH',
+        language='en'
     )
 
--- datasets/Brysbaert-2014c-Concreteness/norare.py
+++ datasets/Brysbaert-2014c-Concreteness/norare.py
@@ -1,13 +1,12 @@
 def download(dataset):
     dataset.download_file(
-        'https://ars.els-cdn.com/content/image/1-s2.0-S0001691814000985-mmc1.xlsx',
-        '1-s2.0-S0001691814000985-mmc1.xlsx'
+        'https://ars.els-cdn.com/content/image/1-s2.0-S0001691814000985-mmc3.xlsx',
+        '1-s2.0-S0001691814000985-mmc3.xlsx'
     )
-
 
 def map(dataset, concepticon, mappings):
     dataset.extract_data(
-        '1-s2.0-S0001691814000985-mmc1.xlsx',
+        '1-s2.0-S0001691814000985-mmc3.xlsx',
         concepticon,
         mappings,
         gloss='DUTCH',
