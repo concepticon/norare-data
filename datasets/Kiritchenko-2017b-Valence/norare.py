2a56109f6be0e7f4c984199bbfce73068cf317b1
Mira A <mira.ahmedovic@gmail.com>
2026-01-28 10:51:47 +0100

    simplified download command and implemented requested changes

--- datasets/Kiritchenko-2017b-Valence/norare.py
+++ datasets/Kiritchenko-2017b-Valence/norare.py
@@ -1,31 +1,8 @@
-import ssl
-from urllib.request import urlopen, Request
-
 def download(dataset):
-    url = 'https://www.saifmohammad.com/WebDocs/bwsvsrs/RS-scores.txt'
-
-    save_dir = dataset.raw_dir
-    save_dir.mkdir(parents=True, exist_ok=True)
-    file_path = save_dir / 'RS-scores.txt'
-
-    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-    ctx.options |= 0x4
-
-    req = Request(
-        url,
-        headers={
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-            'Accept': 'text/plain, text/html, */*',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'identity'
-        }
+    dataset.download_file(
+        'https://www.saifmohammad.com/WebDocs/bwsvsrs/RS-scores.txt',
+        'RS-scores.txt',
     )
-
-    with urlopen(req, context=ctx) as resp:
-        content = resp.read()
-
-    with open(file_path, 'wb') as f:
-        f.write(content)
 
 
 def map(dataset, concepticon, mappings):
