ce559dc7f4a7aa2ecacaca5dfc03df446d8812e5
Mira A <mira.ahmedovic@gmail.com>
2026-05-15 09:07:36 +0200

    adding Hinojosa-2024-Emotions and updating Coso-2023-Emotions

--- datasets/Coso-2023-Emotions/norare.py
+++ datasets/Coso-2023-Emotions/norare.py
@@ -1,8 +1,26 @@
+import ssl
+from urllib.request import urlopen, Request
+from pathlib import Path
+
 def download(dataset):
-    dataset.download_file(
-        'https://figshare.com/ndownloader/files/36434421',
-        'CROWD-5e.xlsx',
-    )
+    URL = "https://ndownloader.figshare.com/files/36434421"
+    FILE_NAME = "CROWD-5e.xlsx"
+
+    save_dir = Path(__file__).resolve().parent / "raw"
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4 
+
+    req = Request(URL, headers={"User-Agent": "Mozilla/5.0"})
+
+    with urlopen(req, context=ctx) as resp:
+        content = resp.read()
+
+    file_path = save_dir / FILE_NAME
+
+    with open(file_path, "wb") as f:
+        f.write(content)
 
 def map(dataset, concepticon, mappings):   
     sheet_list = dataset.get_excel('CROWD-5e.xlsx', 1, dicts=False)
