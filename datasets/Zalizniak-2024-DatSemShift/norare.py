5620f24b463d467e9df5d62bf8c080d424a35b50
Mattis List <mattis.list@uni-passau.de>
2026-08-12 13:45:37 +0200

    update

--- datasets/Zalizniak-2024-DatSemShift/norare.py
+++ datasets/Zalizniak-2024-DatSemShift/norare.py
@@ -1,57 +1,35 @@
-import collections
-from pyconcepticon import models
-from csvw.dsv import reader
 import json
 
+
 def download(dataset):
-    dataset.download_zip(
-        'https://github.com/concepticon/concepticon-data/archive/refs/heads/master.zip',
-        'concepticon-data-master.zip',
-        'concepticon-data-master/concepticondata/conceptlists/Zalizniak-2024-4583.tsv'
+    dataset.download_file(
+        'https://raw.githubusercontent.com/lexibank/datsemshift/refs/tags/v1.1rc/cldf/parameters.csv',
+        "parameters.csv"
     )
 
 def map(dataset, concepticon, mappings):
-    # Load the CLDF-formatted Zalizniak file from the downloaded zip
-    zalizniak = models.Conceptlist.from_file(
-        dataset.raw_dir / "concepticon-data-master" / "concepticondata" / "conceptlists" / "Zalizniak-2024-4583.tsv"
-    )
+    # get mappings
+    zalizniak = {c.number: (c.concepticon_id, c.concepticon_gloss, c.english) for c in
+                 concepticon.conceptlists["Zalizniak-2024-4583"].concepts.values()}
     
-    # Initialize relationship dictionaries
-    target_concepts = {concept.id: [] for concept in zalizniak.concepts.values()}
-    linked_concepts = {concept.id: [] for concept in zalizniak.concepts.values()}
-
-    # JSON parsing helper
-    def parse_json_field(field):
-        try:
-            return json.loads(field) if field else []
-        except json.JSONDecodeError:
-            return []
-
-    # Populate relationship data
-    for concept in zalizniak.concepts.values():
-        tc = parse_json_field(concept.attributes.get("target_concepts", "[]"))
-        lc = parse_json_field(concept.attributes.get("linked_concepts", "[]"))
-        target_concepts[concept.id] = tc
-        linked_concepts[concept.id] = lc
-
-    # Construct output table
+    # the cldf is already formatted as needed, we keep it in this form, just
+    # transform it a bit
     table = []
-    for concept in zalizniak.concepts.values():
-        row = collections.OrderedDict([
-            ('ID', concept.id),
-            ('NUMBER', concept.number),
-            ('CONCEPTICON_ID', concept.concepticon_id),
-            ('CONCEPTICON_GLOSS', concept.concepticon_gloss),
-            ('ENGLISH', concept.english),
-            ('GLOSS_IN_SOURCE', concept.attributes.get('gloss_in_source', '')),
-            ('TARGET_CONCEPTS', target_concepts[concept.id]),
-            ('LINKED_CONCEPTS', linked_concepts[concept.id]),
-            ('SHIFTS', concept.attributes.get('shifts', '')),
-            ('DOMAIN', concept.attributes.get('domain', '')),
-            ('ALIAS', concept.attributes.get('alias', '')),
-            ('DEFINITION', concept.attributes.get('definition', '')),
-        ])
-        table.append(row)
+    for row in dataset.get_csv("parameters.csv", dicts=True, delimiter=","):
+        if row["Number"] in zalizniak:
+            new_row = {"ID": row["ID"]}
+            new_row["CONCEPTICON_ID"] = zalizniak[row["Number"]][0]
+            new_row["CONCEPTICON_GLOSS"] = zalizniak[row["Number"]][1]
+            new_row["ENGLISH"] = zalizniak[row["Number"]][2]
+            if new_row["CONCEPTICON_ID"]:
+                for itm in ["Linked_Concepts", "Target_Concepts", "Domain", "Alias",
+                            "Shifts", "Gloss_in_Source", "Definition"]:
+                    new_row[itm.upper()] = row[itm]
+                    
+                new_row["TARGET_CONCEPTS"] = json.loads(new_row["TARGET_CONCEPTS"])
+                new_row["LINKED_CONCEPTS"] = json.loads(new_row["LINKED_CONCEPTS"])
 
-    # Write to output
+                # add further items that must be p
+                table.append(new_row)
+
     dataset.table.write(table)
